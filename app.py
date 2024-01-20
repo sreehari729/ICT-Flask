843ca86db526c5d4f4e87d9e44a48a1eb74ea768
sreehari729 <sreehari729@gmail.com>
2024-01-20 07:23:27 +0000

    2

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import pickle
 
 app = Flask(__name__)
 @app.route('/')
 def hello():
-    return 'Sreehari'
+    return render_template('index.html')
+
+
+@app.route('/Prediction', methods = ['GET','POST'])
+def predict():
+    if request.method == 'POST':
+        height = request.form['height']
+        print(height)
+        model = pickle.load(open('model.pkl', 'rb'))
+        weight =  model.predict([[float(height)]])
+        print(weight)
+    return render_template('prediction.html', weight = weight)
+
 
 
 if __name__ == '__main__':
